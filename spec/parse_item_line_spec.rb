826eb49155b45fbec065bf176fa18cf8f6a89a32
liyu <1484084728@qq.com>
2016-05-30 01:56:22 +0800

    modify test

--- spec/parse_item_line_spec.rb
+++ spec/parse_item_line_spec.rb
@@ -5,12 +5,20 @@ RSpec.describe ParseItemLine do
   let(:item_line){'1 book at 12.49'}
   let(:parseItemLine){ParseItemLine.new(item_line)}
   describe '#parse' do
-    context 'when give item_line'
-    it 'should return item' do
+    context 'when argument is item_line'
+    it 'should return an item' do
       expect(parseItemLine.parse).class == Item
+    end
+    it 'should have item name' do
       expect(parseItemLine.parse.name).to eq('1 book at')
+    end
+    it 'should have item primary_price' do
       expect(parseItemLine.parse.primary_price).to eq(12.49)
+    end
+    it 'should have item is_special' do
       expect(parseItemLine.parse.is_special).to eq(true)
+    end
+    it 'should have item is_import' do
       expect(parseItemLine.parse.is_import).to eq(false)
     end
   end
